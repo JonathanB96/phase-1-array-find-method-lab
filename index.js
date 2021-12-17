1112bec5fbea83f69c095b4f5e6c45567c5d6ce6
jonathan bola <bolajonathan@yahoo.com>
2021-12-17 15:55:04 -0600

    commit tree e59c4b285b32efe5dda6d86030c3cb2aa07dbfa0

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(arr){
+    
+    arr.find(function(obj){
+        obj.result === "W"
+    })
+    for(let obj of arr){
+        if(obj.result === "W"){
+            return obj.year
+        }
+    }
+  
+}
